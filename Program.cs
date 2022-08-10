be6ad5ee3daa26d6f7cc8eacb0a42f48e5e5d1ff
Sergiy Pavlov <baal_2000@yahoo.com>
2022-08-10 03:00:15 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace RoslynatorAnalyzerNRE;
+
+// Run
+// $ dotnet build -p:DebugAnalyzers=Roslynator.Analyzers -p:TreatWarningsAsErrors=false -p:RunAnalyzersDuringBuild=true /target:rebuild ./RoslynatorAnalyzerNRE/RoslynatorAnalyzerNRE.csproj
+// Get
+// CSC : warning AD0001: Analyzer 'Roslynator.CSharp.Analysis.InvocationExpressionAnalyzer' threw an exception of type 'System.NullReferenceException' with message 'Object reference not set to an instance of an object.'. [/home/user/Repro/RoslynatorAnalyzerNRE/RoslynatorAnalyzerNRE.csproj]
+static class Program
+{
+    static void Main(string[] _)
+    {
+        var strBuilder = new StringBuilder();
+        strBuilder
+            .Append("Hello, World")
+            .Append(DateTime.UtcNow.Millisecond % 2 == 0 ? "??" : '!');
+
+        Console.WriteLine(strBuilder.ToString());
+    }
+}
